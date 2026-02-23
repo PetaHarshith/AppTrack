9c51aa9a487533f5941da79a2916693a66d4c532
Harshith Reddy Peta <157769359+PetaHarshith@users.noreply.github.com>
2026-02-22 22:00:28 -0600

    building the dataprovider + filters

--- Apptrack-frontend/src/constants/index.ts
+++ Apptrack-frontend/src/constants/index.ts
@@ -4,3 +4,5 @@ export const APPLICATION_STATUS_OPTIONS = APPLICATION_STATUSES.map((status) => (
     value: status,
     label: status,
 }));
+
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;

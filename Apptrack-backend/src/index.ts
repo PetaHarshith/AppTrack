bfd7d5051db319a1a1d6174002fa19df940133c7
Harshith Reddy Peta <157769359+PetaHarshith@users.noreply.github.com>
2026-02-22 20:51:35 -0600

    feat: Backend express API

--- Apptrack-backend/src/index.ts
+++ Apptrack-backend/src/index.ts
@@ -1,12 +1,22 @@
 import express from 'express';
+import applicationsRouter from './routes/applications';
+import cors from 'cors';
 
 const app = express();
 const PORT = 8000;
 app.use(express.json());
 
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}))
 app.get('/', (req, res) => {
     res.send('Welcome to the Application Tracking API!');
 });
+
+// Register routes
+app.use('/applications', applicationsRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
